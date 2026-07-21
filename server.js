4e454d4dee7a3516c33a189de96f82908bb3daee
dimarquesmachado-sys <dimarquesmachado@gmail.com>
2026-07-21 19:13:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ app.get('/health', (req, res) => {
   res.json({
     status: 'ok',
     service: 'good-devolucoes-marketplaces-nfsbling',
-    version: '3.73 (2a rodada anti rate-limit - falhas a zero)',
+    version: '3.73.1 (raio-x das 13 falhas)',
     integrations: {
       ml: mlClient.hasToken(),
       bling: blingClient.hasToken(),
