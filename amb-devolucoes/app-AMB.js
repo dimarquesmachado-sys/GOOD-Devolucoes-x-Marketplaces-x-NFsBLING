6baac4750ca4893bf7c690a85416259b664afab4
dimarquesmachado-sys <dimarquesmachado@gmail.com>
2026-07-31 20:24:54 -0300

    Update app-AMB.js

--- amb-devolucoes/app-AMB.js
+++ amb-devolucoes/app-AMB.js
@@ -72,7 +72,7 @@ const impressao = require('./lib-AMB/impressao-AMB');
 const emailAMB = require('./lib-AMB/email-AMB');
 const nfEntrada = require('./lib-AMB/nf-entrada-AMB');
 
-const VERSAO = 'AMB Devolucoes b31';
+const VERSAO = 'AMB Devolucoes b32';
 const SUBIU_EM = new Date().toISOString();
 
 const router = express.Router();
